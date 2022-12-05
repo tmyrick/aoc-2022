3df74dc74cf295245b5338e2b738153e1fdc65ec
tmyrick <travis.myrick@dutchie.com>
2022-12-05 14:19:26 -0800

    day 3.2

---
+++ day_three/two.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+def run
+  input = File.read('input.txt').split("\n")
+  groups = input.each_slice(3).to_a
+  total_priority =
+    groups.sum do |g|
+      calculator(g)
+    end
+
+  binding.pry
+  puts total_priority
+end
+
+def calculator(group)
+  one, two, three = group.map(&:chars)
+  identical_item = one & two & three
+
+  priority_map.index(identical_item.first).to_i
+end
+
+def priority_map
+  @priority_map ||=
+    priority.concat(priority.map(&:upcase))
+            .unshift('')
+end
+
+def priority
+  @priority ||= ('a'..'z').to_a
+end
+
+run
+
