4168a1fb60b774a5d2eb206c8140a175523fe240
tmyrick <travis.myrick@dutchie.com>
2022-12-06 11:59:44 -0800

    day 6.2

--- day_six/one.rb
+++ day_six/one.rb
@@ -2,18 +2,19 @@
 
 require 'pry'
 
-def run
+def run(x)
   input = File.read('input.txt').split("\n").pop.chars
-  current = input[0..3]
-  count = 3
+  current = input[0..x]
+  count = x
 
   loop do
-    break if current.uniq.count == 4
+    break if current.uniq.count == x+1
 
     count += 1
-    current = input[count-3..count]
+    current = input[count-x..count]
   end
   puts count + 1
 end
 
-run
+run(3)
+run(13)
